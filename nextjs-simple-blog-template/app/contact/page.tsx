7e9dfd6e5fc024dbbfb632cde6cf60906bd61ed5
Your Name <you@example.com>
2024-03-07 00:36:37 +0900

    a

--- nextjs-simple-blog-template/app/contact/page.tsx
+++ nextjs-simple-blog-template/app/contact/page.tsx
@@ -2,9 +2,12 @@ import styles from './page.module.css'
 export default function Page() {
   return (
     <div className={styles.main}>
-      <h2>お問い合わせフォーム</h2>
       <form>
-        <input type="text"></input>
+        <div className={styles.label}>メールアドレス</div>
+        <input name="mailAddress" type="text" maxLength={100} className={styles.input} />
+        <div className={styles.label}>お問い合わせ内容</div>
+        <textarea name="comment" maxLength={500} rows={5} className={styles.input} />
+        <button className={styles.button}>送信</button>
       </form>
     </div>
 
